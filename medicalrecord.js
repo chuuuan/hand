08395393e56dc039c492c881aa227939e2b8744b
崔緯平 <gino930122@gmail.com>
2019-05-25 11:55:29 +0800

    抓到使用者

--- medicalrecord.js
+++ medicalrecord.js
@@ -4,6 +4,24 @@ document.getElementById('date2').value = date2;
 document.getElementById('Submit1').addEventListener('click', savedata);
 var db = firebase.firestore();
 
+var docRef = db.collection("medic").doc(identify2);
+
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+      document.getElementById('fname').value = doc.data().name;
+      document.getElementById('phonename').value = doc.data().phone;
+      document.getElementById('medical').value = doc.data().medical;
+      document.getElementById('allergic').value = doc.data().allergic;
+      document.getElementById('lbox').value = doc.data().medbox;
+      document.getElementById('subject').value = doc.data().txtbox;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
+
 
 function savedata() {
 
